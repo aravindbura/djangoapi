35f24f3dc1d7720e04cadf625bb9d1ce68dbf91e
Aravind <aravindbura1996@gmail.com>
2021-07-02 21:43:50 +0530

    rest1

--- courses/views.py
+++ courses/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Restaurant
 from .serializer import RestaurantSerializer
-
+  
 class Restaurantview(viewsets.ModelViewSet):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
